3833a0ad0f2c213e6e24119e52db4f6a79ba0078
심철환 <a@akaiv.com>
2016-06-01 23:57:55 +0900

    자동화: Grunt copy를 이용해 부트스트랩 v4-alpha.2의 SCSS 파일들 복사

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,23 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  'use strict';
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      bootstrap: {
+        expand: true,
+        cwd: 'assets/bootstrap/scss/',
+        src: ['**/*'],
+        dest: '_sass/vendors/bootstrap/'
+      }
+    }
+
   });
 
-  grunt.registerTask('build', []);
+  require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+  require('time-grunt')(grunt);
+
+  grunt.registerTask('build', ['copy']);
 
 };
