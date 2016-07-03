0acd8fda8db8a8e1e245e6073d916d280730b731
심철환 <a@akaiv.com>
2016-07-04 01:29:39 +0900

    스크립트: 외부 링크들이 새 탭에서 열리도록 설정

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,5 +1,10 @@
 (function ($) {
   'use strict';
 
-  $('[data-toggle="tooltip"]').tooltip();
+  // 외부 링크들이 새 탭에서 열리도록 설정
+  $('a[href]').each(function () {
+    if (location.host != $(this).prop('host')) {
+      $(this).attr('target', '_blank');
+    }
+  });
 })(jQuery);
