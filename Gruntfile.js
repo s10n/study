98423d031d2100317da093293d82632bcb70ff87
심철환 <a@akaiv.com>
2016-07-04 01:29:27 +0900

    스크립트: 부트스트랩 스크립트를 아직 사용하지 않음

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,19 @@
 module.exports = function (grunt) {
   'use strict';
 
-  var bootstrapScripts = [
-    'assets/bootstrap/js/dist/util.js',
-    'assets/bootstrap/js/dist/alert.js',
-    'assets/bootstrap/js/dist/button.js',
-    'assets/bootstrap/js/dist/carousel.js',
-    'assets/bootstrap/js/dist/collapse.js',
-    'assets/bootstrap/js/dist/dropdown.js',
-    'assets/bootstrap/js/dist/modal.js',
-    'assets/bootstrap/js/dist/scrollspy.js',
-    'assets/bootstrap/js/dist/tab.js',
-    'assets/bootstrap/js/dist/tooltip.js',
-    'assets/bootstrap/js/dist/popover.js'
-  ];
+  // var bootstrapScripts = [
+  //   'assets/bootstrap/js/dist/util.js',
+  //   'assets/bootstrap/js/dist/alert.js',
+  //   'assets/bootstrap/js/dist/button.js',
+  //   'assets/bootstrap/js/dist/carousel.js',
+  //   'assets/bootstrap/js/dist/collapse.js',
+  //   'assets/bootstrap/js/dist/dropdown.js',
+  //   'assets/bootstrap/js/dist/modal.js',
+  //   'assets/bootstrap/js/dist/scrollspy.js',
+  //   'assets/bootstrap/js/dist/tab.js',
+  //   'assets/bootstrap/js/dist/tooltip.js',
+  //   'assets/bootstrap/js/dist/popover.js'
+  // ];
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
     concat: {
       core: {
         src: [
-          bootstrapScripts,
+          // bootstrapScripts,
           '_scripts/main.js'
         ],
         dest: 'js/main.js'
